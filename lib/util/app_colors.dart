9491393ff9fd367230a27251776472f4ae13833b
stanley <starnleymbote@gmail.com>
2023-06-21 19:54:01 +0300

    added the login button and login with google button

--- lib/util/app_colors.dart
+++ lib/util/app_colors.dart
@@ -3,5 +3,9 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color(0xFF00BCD4);
   static const Color buttonColor = Color(0xFFFF5722);
-  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color backgroundColor = Color(0xFEFEFEFE);
+  static const Color textColor = Color(0xAA000437);
+  static const Color border = Color(0xAA000437);
+  static const Color textClr = Color(0xAA000078);
+  static const Color textClr2 = Color(0xAA3480eb);
 }
